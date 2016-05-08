680c12857ca9b73ca17e7bc5c3bdb0b0f3597bb8
Tomomi <tomomi.tamura2486@gmail.com>
2016-05-08 15:54:18 -0700

    first commit

--- cat_Game/GameScene.swift
+++ cat_Game/GameScene.swift
@@ -6,39 +6,73 @@
 //  Copyright (c) 2016 Tomomi Tamura. All rights reserved.
 //
 
-import SpriteKit
+typealias CMAccelerometerHandler = (CMAccelerometerData?, NSError?) -> Void
 
-class GameScene: SKScene {
+import SpriteKit
+import CoreMotion
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    let manager = CMMotionManager()
+    var player = SKSpriteNode()
+    //var dog = SKSpriteNode()
+    var endNode = SKSpriteNode()
+    var welcomeMessage = SKLabelNode()
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        self.physicsWorld.contactDelegate = self
+        
+        player = self.childNodeWithName("player") as! SKSpriteNode
+        
+        endNode = self.childNodeWithName("endNode") as! SKSpriteNode
+        
+        //dog = self.childNodeWithName("dog") as! SKSpriteNode
+        
+        welcomeMessage = self.childNodeWithName("welcomeMessage") as! SKLabelNode
+        welcomeMessage.fontColor = UIColor.greenColor();
+        welcomeMessage.text = "Welcome Home, Ruki-kun!"
+        welcomeMessage.fontSize = 45
+        welcomeMessage.position = CGPoint(x: 500, y: 1600)
+        self.welcomeMessage.hidden = true
+
+        
+        manager.startAccelerometerUpdates()
+        if manager.accelerometerAvailable
+        {
+            manager.accelerometerUpdateInterval = 0.1
+        }
+        
+        manager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()){ (data, error) in
+            
+            self.physicsWorld.gravity = CGVectorMake(CGFloat((data?.acceleration.x)!) * 10, CGFloat((data?.acceleration.y)!) * 10)
+            
+        }
+        
+    }
+    func didBeginContact(contact: SKPhysicsContact){
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
+        
+        if bodyA.categoryBitMask == 1 && bodyB.categoryBitMask == 2 || bodyA.categoryBitMask == 2 && bodyB.categoryBitMask == 1{
+            //end message
+            print("You won!")
+            welcomeMessage.text = "Welcome Home!"
+            welcomeMessage.hidden = false
+            manager.accelerometerUpdateInterval = 0.5
+
+        }
+        else{
+            welcomeMessage.hidden = true
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
